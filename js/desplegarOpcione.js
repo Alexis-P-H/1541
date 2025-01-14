12364b2dd665cee2bbc82ec9c1ecf41f3a152810
Alexis Padierna Higuita <alexis.padierna@cadena.com.co>
2025-01-14 08:28:27 -0500

    diojmio

--- js/desplegarOpcione.js
+++
@@ -1,9 +0,0 @@
- // Seleccionamos el texto desplegable y el contenido oculto
- const textoDesplegable = document.getElementById('texto-desplegable');
- const contenidoOculto = document.querySelector('.contenido-oculto');
-
- // Agregamos el evento de clic
- textoDesplegable.addEventListener('click', () => {
-   // Alternamos la clase 'activo' para mostrar u ocultar el contenido
-   contenidoOculto.classList.toggle('activo');
- });
